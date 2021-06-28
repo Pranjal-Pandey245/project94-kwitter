f18d5b032b547b158f0e5e2fd886d497ff396416
Pranjal-Pandey245 <81172387+Pranjal-Pandey245@users.noreply.github.com>
2021-06-28 15:00:02 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyDNQ3zVUFJcu-QD-vPkjy4lylcldlnRC4A",
+    authDomain: "kwitter-project-8f05c.firebaseapp.com",
+    databaseURL: "https://kwitter-project-8f05c-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-8f05c",
+    storageBucket: "kwitter-project-8f05c.appspot.com",
+    messagingSenderId: "540595829795",
+    appId: "1:540595829795:web:cf57c292d556507e8d0596"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
