44036d4dcde50e48838ac3b95651f53648d265a0
Pranjal-Pandey245 <81172387+Pranjal-Pandey245@users.noreply.github.com>
2021-07-01 15:05:11 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -8,5 +8,44 @@
     messagingSenderId: "540595829795",
     appId: "1:540595829795:web:cf57c292d556507e8d0596"
   };
+
+
+
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  user_name=localStorage.getItem("user=");
+  document.getElementById("user_name").innerHTML=user_name;
+
+  function add_room(){
+
+    room_name=document.getElementById("room_name").value;
+    localStorage.setItem("room" , room_name);
+
+    firebase.database().ref("/").child(room_name).update({
+      purpose: "adding room name"
+});
+    
+  }
+
+  function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+    Room_names = childKey;
+
+    console.log(Room_names);
+    row="<div class='room_name' id="+Room_names+" onclick='redirect_room(this.id)' > #"+Room_names+"</div> <hr>";
+    document.getElementById("output").innerHTML+=row;
+
+  });});}
+  getData();
+
+  function redirect_room(name){
+    console.log(name);
+    localStorage.setItem("name-", name);
+    window.location="kwitter_page.html";
+  }
+
+  function logout(){
+    localStorage.removeItem("user=");
+    localStorage.removeItem("name-");
+    window.location="index.html";
+  }
